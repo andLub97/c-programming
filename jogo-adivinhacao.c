2193f7f56a104dcd1d3d7e33647c510b486e1c98
André Lubawski <andrelubawski97@outlook.com>
2022-02-15 10:58:04 -0300

    iniciando a revisão de C com um jogo de adivinhação

---
+++ jogo-adivinhacao.c
@@ -0,0 +1,14 @@
+/*
+    Para executar o programa no windows
+    1: instale o mingw https://pt.wikihow.com/Compilar-um-Programa-Usando-o-Compilador-GNU-(GCC)
+    2: no terminal, estando no diretório do arquivo criado, digite
+                        gcc jogo-adivinhacao.c -o jogo-adivinhacao.exe
+    3: após compilar, digite 
+                        ./jogo-adivinhacao.exe
+*/
+
+#include <stdio.h>
+
+int main(){
+    printf("Vamos jogar!");
+}
